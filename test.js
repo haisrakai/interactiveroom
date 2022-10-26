422ee714dcd3877230b6559f726e5a730835aa40
haisrakai <shyekyra@gmail.com>
2022-10-26 17:25:26 +0800

    my first

---
+++ test.js
@@ -0,0 +1,21 @@
+const sechand = document.querySelector("[data-second]")
+const minhand = document.querySelector("[data-minute]")
+const hourhand = document.querySelector("[data-hour]")
+function setClock(){
+    console.log("uh huh")
+    const currentDate = new Date()
+    let seconds = currentDate.getSeconds() / 60
+    let minutes = currentDate.getMinutes()
+    let hours = currentDate.getHours()
+    console.log(seconds)
+    setRotation(sechand, seconds)
+    setRotation(minhand, minutes)
+    setRotation(hourhand, hours)
+}
+
+function setRotation(hand, rotation){
+    hand.style.setProperty('--rotation', rotation * 360)
+}
+
+setInterval(setClock, 1000)
+setClock()
